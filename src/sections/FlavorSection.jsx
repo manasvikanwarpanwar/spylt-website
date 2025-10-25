9585f31f4dc822742cc81974e4963d8acef747eb
manasvi kanwar panwar <user.email: manasvikanwarpanwar711@gmail.com>
2025-10-25 20:34:22 +0530

    Fix filename case for Netlify build

--- src/sections/FlavorSection.jsx
+++ src/sections/FlavorSection.jsx
@@ -7,7 +7,7 @@ const FlavorSection = () => {
     <section className="flavor-section">
       <div className="h-full flex lg:flex-row flex-col items-center relative">
         <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0">
-        <Flavortitle/>
+        <FlavorTitle/>
         </div>
         <div className="h-full">
           <FlavorSlider/>
